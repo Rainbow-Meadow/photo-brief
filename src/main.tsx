b2244bfae92ea8cd7696e1c3be6d9ff86ebf2e4d
Rainbow Meadow Memorial Park <76604693+prberthiaume@users.noreply.github.com>
2026-05-03 14:58:56 -0400

    Load final brand override stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./theme.css";
+import "./brand-overrides.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
